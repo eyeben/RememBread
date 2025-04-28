45fee9d3595f450d4a78620f1b0f0a80231e210b
최원서 <cws98tiger@naver.com>
2025-04-28 16:19:47 +0900

    [FE] Feat: Add favicon (S12P31A704-181)

--- RememBread_frontend/src/components/indexCardView/IndexCardSearchBar.tsx
+++ RememBread_frontend/src/components/indexCardView/IndexCardSearchBar.tsx
@@ -1,31 +1,18 @@
-import { Search } from "lucide-react";
 import { useState } from "react";
+import { Search } from "lucide-react";
 
 const IndexCardSearchBar = () => {
   const [query, setQuery] = useState<string>("");
 
   return (
-    <div className="flex items-center border rounded-full px-4 py-2 w-full">
-      {/* <svg
-        className="w-4 h-4 text-gray-400 mr-2"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M21 21l-4.35-4.35M10.5 18a7.5 7.5 0 1 0 0-15 7.5 7.5 0 0 0 0 15z"
-        />
-      </svg> */}
-      <Search className="w-5 h-full " />
+    <div className="flex items-center border rounded-full px-4 py-2 w-full gap-2">
+      <Search className="w-5 h-full text-neutral-400" />
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="검색어를 입력하세요"
-        className="outline-none w-full text-sm text-gray-700 placeholder-gray-400"
+        className="outline-none w-full text-sm text-netural-700 placeholder-gray-400"
       />
     </div>
   );
