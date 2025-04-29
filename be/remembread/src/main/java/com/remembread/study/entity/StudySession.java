9a0a67cbde52e95ad964443efe2594d452b8353c
송정호 <dynamite885@naver.com>
2025-04-29 18:00:33 +0900

    [BE] Feat: Add StudySession entity (S12P31A704-192)

---
+++ be/remembread/src/main/java/com/remembread/study/entity/StudySession.java
@@ -0,0 +1,31 @@
+package com.remembread.study.entity;
+
+import com.remembread.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.locationtech.jts.geom.LineString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "study_sessions")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class StudySession {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "studied_at", nullable = false)
+    private LocalDateTime studiedAt = LocalDateTime.now();
+
+    @Column(columnDefinition = "geometry(LINESTRING, 4326)")
+    private LineString route;
+}
