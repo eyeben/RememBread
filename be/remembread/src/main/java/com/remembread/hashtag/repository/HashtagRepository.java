b925511ef03ac0887fefcddff551cb9f43647d5b
송정호 <dynamite885@naver.com>
2025-04-30 17:34:26 +0900

    [BE] Feat: Add findAllNamesByCardSetId query to HashtagRepository (S12P31A704-198)

--- be/remembread/src/main/java/com/remembread/hashtag/repository/HashtagRepository.java
+++ be/remembread/src/main/java/com/remembread/hashtag/repository/HashtagRepository.java
@@ -2,9 +2,22 @@ package com.remembread.hashtag.repository;
 
 import com.remembread.hashtag.entity.Hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
     boolean existsByName(String name);
 
     Hashtag findOneByName(String name);
+
+    @Query(nativeQuery = true,
+            value = "SELECT h.name " +
+                    "FROM hashtags h " +
+                    "JOIN card_set_hashtags ch " +
+                    "ON h.id = ch.hashtag_id " +
+                    "WHERE ch.card_set_id = :cardSetId "
+    )
+    List<String> findAllNamesByCardSetId(@Param("cardSetId") Long id);
 }
