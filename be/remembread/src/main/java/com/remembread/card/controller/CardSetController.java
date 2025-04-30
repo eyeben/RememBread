78b488514f3f50cc141ecf9e495bdc3e2caf9e01
송정호 <dynamite885@naver.com>
2025-04-30 16:22:09 +0900

    [BE] Feat: Add createCardSet method to CardSetController (S12P31A704-193)

--- be/remembread/src/main/java/com/remembread/card/controller/CardSetController.java
+++ be/remembread/src/main/java/com/remembread/card/controller/CardSetController.java
@@ -1,7 +1,13 @@
 package com.remembread.card.controller;
 
+import com.remembread.apipayload.ApiResponse;
+import com.remembread.auth.annotation.AuthUser;
+import com.remembread.card.dto.request.CardSetCreateRequest;
 import com.remembread.card.service.CardSetService;
+import com.remembread.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CardSetController {
     private final CardSetService cardSetService;
+
+    @PostMapping
+    public ApiResponse<Void> createCardSet(
+            @RequestBody CardSetCreateRequest request,
+            @AuthUser User user
+            ) {
+        cardSetService.createCardSet(request, user);
+        return ApiResponse.onSuccess(null);
+    }
 }
