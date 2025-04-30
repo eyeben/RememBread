be1be6f149324df80a37a2c24af2b98d64b3b392
송정호 <dynamite885@naver.com>
2025-04-30 11:00:40 +0900

    [BE] Feat: Add createFolder method to FolderController (S12P31A704-186)

--- be/remembread/src/main/java/com/remembread/card/controller/FolderController.java
+++ be/remembread/src/main/java/com/remembread/card/controller/FolderController.java
@@ -1,7 +1,13 @@
 package com.remembread.card.controller;
 
+import com.remembread.apipayload.ApiResponse;
+import com.remembread.auth.annotation.AuthUser;
+import com.remembread.card.dto.request.FolderCreateRequest;
 import com.remembread.card.service.FolderService;
+import com.remembread.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class FolderController {
     private final FolderService folderService;
+
+    @PostMapping
+    public ApiResponse<Void> createFolder(
+            @RequestBody FolderCreateRequest request,
+            @AuthUser User user) {
+        folderService.createFolder(request, user);
+        return ApiResponse.onSuccess(null);
+    }
 }
