f0283e946ea5a7eaaaa785003f71a94aa6fd7e8e
kimjimin <kimjm9841@naver.com>
2025-04-30 13:31:23 +0900

    [BE] Feat: Add isAgreedTerms Column (S12P31A704-177)

--- be/remembread/src/main/java/com/remembread/auth/entity/UserTokens.java
+++ be/remembread/src/main/java/com/remembread/auth/entity/UserTokens.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class UserTokens {
-    private final Boolean isNew;
+    private final Boolean isAgreedTerms;
     private final String userId;
     private final String refreshToken;
     private final String accessToken;
