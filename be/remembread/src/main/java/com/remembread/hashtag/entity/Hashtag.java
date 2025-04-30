bb05b2c479be9e4d906fc1b22605706a01ab0441
송정호 <dynamite885@naver.com>
2025-04-30 14:24:11 +0900

    [BE] Chore: Rename table 'hashtag' to 'hashtags' (S12P31A704-193)

--- be/remembread/src/main/java/com/remembread/hashtag/entity/Hashtag.java
+++ be/remembread/src/main/java/com/remembread/hashtag/entity/Hashtag.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "hashtag")
+@Table(name = "hashtags")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
