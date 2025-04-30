38547364ca6cb5b79fa0960ad6302843d376f81d
송정호 <dynamite885@naver.com>
2025-04-30 13:24:55 +0900

    [BE] Feat: Add getFolderInfo method to FolderController (S12P31A704-186)

--- be/remembread/src/main/java/com/remembread/card/controller/FolderController.java
+++ be/remembread/src/main/java/com/remembread/card/controller/FolderController.java
@@ -3,13 +3,11 @@ package com.remembread.card.controller;
 import com.remembread.apipayload.ApiResponse;
 import com.remembread.auth.annotation.AuthUser;
 import com.remembread.card.dto.request.FolderCreateRequest;
+import com.remembread.card.dto.response.FolderResponse;
 import com.remembread.card.service.FolderService;
 import com.remembread.user.entity.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/folders")
@@ -23,5 +21,11 @@ public class FolderController {
             @AuthUser User user) {
         folderService.createFolder(request, user);
         return ApiResponse.onSuccess(null);
+    }
+
+    @GetMapping("/{folderId}")
+    public ApiResponse<FolderResponse> getFolderInfo(@PathVariable Long folderId) {
+        FolderResponse response = folderService.getFolderInfo(folderId);
+        return ApiResponse.onSuccess(response);
     }
 }
