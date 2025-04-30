a441e412af9a8d758a8c4b0528b5df8c7e5e9d2a
송정호 <dynamite885@naver.com>
2025-04-30 14:14:20 +0900

    [BE] Feat: Add Hashtag entity (S12P31A704-193)

---
+++ be/remembread/src/main/java/com/remembread/hashtag/entity/Hashtag.java
@@ -0,0 +1,20 @@
+package com.remembread.hashtag.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "hashtag")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Hashtag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 10, nullable = false, unique = true)
+    private String name;
+}
