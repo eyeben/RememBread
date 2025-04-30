7b6481fb6a241d42c4ac12b6e7a2ce5d676e5c46
eyeben <qkrehdals97@naver.com>
2025-04-30 11:12:32 +0900

    [BE] Refactor: Card-create-many(S12P31A704-157)
    init project

--- be/remembread/src/main/java/com/remembread/card/controller/CardController.java
+++ be/remembread/src/main/java/com/remembread/card/controller/CardController.java
@@ -1,6 +1,6 @@
 package com.remembread.card.controller;
 import com.remembread.apipayload.ApiResponse;
-import com.remembread.card.dto.request.CardCreateGetDto;
+import com.remembread.card.dto.request.CardCreateGetRequest;
 import com.remembread.card.service.CardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +15,7 @@ public class CardController {
     private final CardService cardService;
 
     @PostMapping("")
-    public ApiResponse<?> createCard(@RequestBody CardCreateGetDto request) {
+    public ApiResponse<?> createCard(@RequestBody CardCreateGetRequest request) {
         cardService.createCard(request, null);// 로그인 구현 후 변경
         return ApiResponse.onSuccess(null);
     }
