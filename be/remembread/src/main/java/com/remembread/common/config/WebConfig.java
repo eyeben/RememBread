f1e6c380b7ee90bd8547a0e051ebf36d91f9118b
kimjimin <kimjm9841@naver.com>
2025-04-30 12:33:47 +0900

    [BE] Feat: Add WebConfig (S12P31A704-177)

---
+++ be/remembread/src/main/java/com/remembread/common/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.remembread.common.config;
+
+import com.remembread.auth.AuthUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthUserArgumentResolver authUserArgumentResolver;
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(authUserArgumentResolver);
+    }
+}
+
