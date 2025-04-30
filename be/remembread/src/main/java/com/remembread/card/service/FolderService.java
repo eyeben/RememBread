bf993a159397c190d80f677e6496e108ed5b06e2
송정호 <dynamite885@naver.com>
2025-04-30 10:52:44 +0900

    [BE] Feat: Add createFolder method to FolderService (S12P31A704-186)

--- be/remembread/src/main/java/com/remembread/card/service/FolderService.java
+++ be/remembread/src/main/java/com/remembread/card/service/FolderService.java
@@ -1,11 +1,30 @@
 package com.remembread.card.service;
 
+import com.remembread.card.dto.request.FolderCreateRequest;
+import com.remembread.card.entity.Folder;
 import com.remembread.card.repository.FolderRepository;
+import com.remembread.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
 public class FolderService {
     private final FolderRepository folderRepository;
+
+    public void createFolder(FolderCreateRequest request, User user) {
+        Folder upper = null;
+        if (request.getUpperFolderId() != null) {
+            upper = folderRepository.getReferenceById(request.getUpperFolderId());
+        }
+        Folder folder = Folder.builder()
+                .upperFolder(upper)
+                .subFolders(new ArrayList<Folder>())
+                .user(user)
+                .name(request.getName())
+                .build();
+        folderRepository.save(folder);
+    }
 }
