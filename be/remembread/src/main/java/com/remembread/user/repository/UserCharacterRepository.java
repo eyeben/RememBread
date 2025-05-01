8d6d938be2fe44b2b5abfcfa90a6103dbb657005
kimjimin <kimjm9841@naver.com>
2025-05-01 22:07:14 +0900

    [BE] Feat: Implement User Character Info and Withdraw User API (S12P31A704-177)

--- be/remembread/src/main/java/com/remembread/user/repository/UserCharacterRepository.java
+++ be/remembread/src/main/java/com/remembread/user/repository/UserCharacterRepository.java
@@ -1,7 +1,12 @@
 package com.remembread.user.repository;
 
+import com.remembread.user.entity.Character;
+import com.remembread.user.entity.User;
 import com.remembread.user.entity.UserCharacter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserCharacterRepository extends JpaRepository<UserCharacter, Long> {
+    Optional<UserCharacter> findByUserAndCharacter(User user, Character character);
 }
