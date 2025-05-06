5879a8b22c444dd1faf201cae811e853728c154a
송정호 <dynamite885@naver.com>
2025-05-06 13:46:26 +0900

    [BE] Feat: Add startStudySession method to StudyController (S12P31A704-208)

--- be/remembread/src/main/java/com/remembread/study/controller/StudyController.java
+++ be/remembread/src/main/java/com/remembread/study/controller/StudyController.java
@@ -1,7 +1,13 @@
 package com.remembread.study.controller;
 
+import com.remembread.apipayload.ApiResponse;
+import com.remembread.auth.annotation.AuthUser;
+import com.remembread.study.dto.StudyStartRequest;
 import com.remembread.study.service.StudyService;
+import com.remembread.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class StudyController {
     private final StudyService studyService;
+
+    @GetMapping("/{cardSetId}/start")
+    public ApiResponse<Void> startStudySession(
+            @PathVariable("cardSetId") Long cardSetId,
+            StudyStartRequest request,
+            @AuthUser User user
+            ) {
+        studyService.startStudySession(cardSetId, request, user);
+        return ApiResponse.onSuccess(null);
+    }
 }
