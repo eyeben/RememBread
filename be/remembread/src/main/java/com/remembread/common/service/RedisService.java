bb1fb41a1460ab92da59224e901ae237239d8d6e
송정호 <dynamite885@naver.com>
2025-05-06 13:29:04 +0900

    [BE] Feat: Add ZSet methods to RedisService (S12P31A704-208)

--- be/remembread/src/main/java/com/remembread/common/service/RedisService.java
+++ be/remembread/src/main/java/com/remembread/common/service/RedisService.java
@@ -5,6 +5,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -31,4 +32,15 @@ public class RedisService {
     public boolean hasKey(String key) {
         return Boolean.TRUE.equals(redisTemplate.hasKey(key));
     }
+
+    public void addToZSet(String key, String value, double score) {
+        redisTemplate.opsForZSet().add(key, value, score);
+    }
+
+    public String top(String key) {
+        return Objects.requireNonNull(redisTemplate.opsForZSet().range(key, 0, 0))
+                .stream()
+                .findFirst().orElse(null);
+    }
+
 }
