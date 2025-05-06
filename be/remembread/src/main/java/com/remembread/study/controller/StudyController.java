57336b49cab59322cad67fe552c047edf58cca0e
송정호 <dynamite885@naver.com>
2025-05-07 00:48:26 +0900

    [BE] Feat: Add stopStudySession method to StudyController (S12P31A704-208)

--- be/remembread/src/main/java/com/remembread/study/controller/StudyController.java
+++ be/remembread/src/main/java/com/remembread/study/controller/StudyController.java
@@ -3,13 +3,11 @@ package com.remembread.study.controller;
 import com.remembread.apipayload.ApiResponse;
 import com.remembread.auth.annotation.AuthUser;
 import com.remembread.study.dto.request.StudyStartRequest;
+import com.remembread.study.dto.request.StudyStopRequest;
 import com.remembread.study.service.StudyService;
 import com.remembread.user.entity.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/studies")
@@ -17,13 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudyController {
     private final StudyService studyService;
 
-    @GetMapping("/{cardSetId}/start")
+    @PostMapping("/{cardSetId}/start")
     public ApiResponse<Void> startStudySession(
             @PathVariable("cardSetId") Long cardSetId,
             StudyStartRequest request,
             @AuthUser User user
             ) {
         studyService.startStudySession(cardSetId, request, user);
+        return ApiResponse.onSuccess(null);
+    }
+
+    @PostMapping("/{cardSetId}/stop")
+    public ApiResponse<Void> stopStudySession(
+            @PathVariable("cardSetId") Long cardSetId,
+            StudyStopRequest request,
+            @AuthUser User user
+            ) {
+        studyService.stopStudySession(cardSetId, request, user);
         return ApiResponse.onSuccess(null);
     }
 }
