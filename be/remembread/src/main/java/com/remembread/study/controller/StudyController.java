f91b153f2262203ed7aca601fa876282c3e6a0e8
송정호 <dynamite885@naver.com>
2025-05-08 05:06:07 +0900

    [BE] Feat: Add study methods to StudyController (S12P31A704-281)

--- be/remembread/src/main/java/com/remembread/study/controller/StudyController.java
+++ be/remembread/src/main/java/com/remembread/study/controller/StudyController.java
@@ -3,8 +3,10 @@ package com.remembread.study.controller;
 import com.remembread.apipayload.ApiResponse;
 import com.remembread.auth.annotation.AuthUser;
 import com.remembread.card.dto.response.CardResponse;
+import com.remembread.study.dto.request.AnswerResultRequest;
 import com.remembread.study.dto.request.StudyStartRequest;
 import com.remembread.study.dto.request.StudyStopRequest;
+import com.remembread.study.dto.response.RemainingCardCountResponse;
 import com.remembread.study.service.StudyService;
 import com.remembread.user.entity.User;
 import lombok.RequiredArgsConstructor;
@@ -33,5 +35,23 @@ public class StudyController {
             ) {
         studyService.stopStudySession(cardSetId, request, user);
         return ApiResponse.onSuccess(null);
+    }
+
+    @PostMapping("/{cardSetId}/cards/{cardId}/answer")
+    public ApiResponse<RemainingCardCountResponse> submitAnswer(
+            @PathVariable Long cardSetId,
+            @PathVariable Long cardId,
+            AnswerResultRequest request,
+            @AuthUser User user
+    ) {
+        return ApiResponse.onSuccess(studyService.submitAnswer(cardSetId, cardId, request, user));
+    }
+
+    @GetMapping("/{cardSetId}/next")
+    public ApiResponse<CardResponse> getNextCard(
+            @PathVariable Long cardSetId,
+            @AuthUser User user
+    ) {
+        return ApiResponse.onSuccess(studyService.getNextCard(cardSetId, user));
     }
 }
