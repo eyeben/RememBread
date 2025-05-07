5c1b1e01c51cc1e9b5228e4ee3cbf12fced9d57f
eyeben <qkrehdals97@naver.com>
2025-05-07 12:23:14 +0900

    [BE] Feat: Card-set-Simple-List implemented and tested (S12P31A704-277)
    Card-set-Simple-List implemented and tested

--- be/remembread/src/main/java/com/remembread/card/controller/CardSetController.java
+++ be/remembread/src/main/java/com/remembread/card/controller/CardSetController.java
@@ -4,11 +4,8 @@ import com.remembread.apipayload.ApiResponse;
 import com.remembread.auth.annotation.AuthUser;
 import com.remembread.card.dto.request.CardSetCreateRequest;
 import com.remembread.card.dto.request.CardSetUpdateRequest;
-import com.remembread.card.dto.response.CardListResponse;
-import com.remembread.card.dto.response.CardSetListGetResponse;
-import com.remembread.card.dto.response.CardSetResponse;
+import com.remembread.card.dto.response.*;
 import com.remembread.card.dto.request.ForkCardSetRequest;
-import com.remembread.card.dto.response.CardSetSearchResponse;
 import com.remembread.card.enums.CardSetSortType;
 import com.remembread.card.enums.SearchCategory;
 import com.remembread.card.service.CardSetService;
@@ -83,11 +80,11 @@ public class CardSetController {
 
     @GetMapping("/lists")
     @Operation(summary = "카드셋 목록 조회", description = "폴더 ID를 기준으로 카드셋 목록을 페이징 조회합니다.")
-    public ApiResponse<CardSetListGetResponse> getCardSetLists(@Parameter(description = "카드셋을 조회할 폴더 ID", example = "1", required = true) @RequestParam Long folderId,
-                                                           @Parameter(description = "페이지 번호 (0부터 시작)", example = "0", required = true) @RequestParam(defaultValue = "0") int page,
-                                                           @Parameter(description = "페이지당 항목 수", example = "10", required = true) @RequestParam(defaultValue = "10") int size,
-                                                           @Parameter(description = "정렬 기준 (예: 최신순, 인기순, 포크순)", example = "최신순", required = true) @RequestParam(defaultValue = "최신순") String sort,
-                                                           @AuthUser User user) {
+    public ApiResponse<CardSetListGetResponse> getCardSetList(@Parameter(description = "카드셋을 조회할 폴더 ID", example = "1", required = true) @RequestParam Long folderId,
+                                                              @Parameter(description = "페이지 번호 (0부터 시작)", example = "0", required = true) @RequestParam(defaultValue = "0") int page,
+                                                              @Parameter(description = "페이지당 항목 수", example = "10", required = true) @RequestParam(defaultValue = "10") int size,
+                                                              @Parameter(description = "정렬 기준 (예: 최신순, 인기순, 포크순)", example = "최신순", required = true) @RequestParam(defaultValue = "최신순") String sort,
+                                                              @AuthUser User user) {
     return ApiResponse.onSuccess(cardSetService.getCardSetList(folderId, page, size, sort, user.getId()));
     }
 
@@ -102,5 +99,12 @@ public class CardSetController {
 
 
         return ApiResponse.onSuccess(cardSetService.searchCardSets(searchCategory, query, page, size, cardSetSortType));
+    }
+
+    @GetMapping("/lists-simple")
+    @Operation(summary = "카드셋 목록 조회", description = "폴더 ID를 기준으로 카드셋 목록을 페이징 조회합니다.")
+    public ApiResponse<CardSetSimpleListGetResponse> getCardSetSimpleList(@Parameter(description = "카드셋을 조회할 폴더 ID", example = "1", required = true) @RequestParam Long folderId,
+                                                                          @AuthUser User user) {
+        return ApiResponse.onSuccess(cardSetService.getCardSetSimpleList(folderId, user.getId()));
     }
 }
