8a36026e0404c9d53779e660ebe605ccf168b348
이성준 <akacast01@gmail.com>
2025-05-07 14:22:58 +0900

    [FE] Feat: Add get subfolder API (S12P31A704-294)

--- RememBread_frontend/src/services/folder.ts
+++ RememBread_frontend/src/services/folder.ts
@@ -11,12 +11,3 @@ export const getSubFolder = async (folderId: number) => {
   const response = await http.get(`/folders/${folderId}/sub-folders`);
   return response.data;
 };
-
-// 폴더 생성
-export const postFolder = async (name: string, upperFolderId: number | null) => {
-  const response = await http.post("/folders", {
-    name,
-    upperFolderId,
-  });
-  return response.data;
-};
