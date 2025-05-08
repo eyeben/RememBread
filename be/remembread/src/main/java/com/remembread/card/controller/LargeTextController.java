3d3803c90779c37de2a59926f00ce08d08d232cd
kimjimin <kimjm9841@naver.com>
2025-05-08 10:11:15 +0900

    [BE] Feat: Implement Large Text Stream API (S12P31A704-180)

--- be/remembread/src/main/java/com/remembread/card/controller/LargeTextController.java
+++ be/remembread/src/main/java/com/remembread/card/controller/LargeTextController.java
@@ -4,13 +4,13 @@ import com.remembread.apipayload.ApiResponse;
 import com.remembread.auth.annotation.AuthUser;
 import com.remembread.card.dto.response.CardResponse;
 import com.remembread.card.service.LargeTextService;
+import com.remembread.common.service.GPTService;
 import com.remembread.user.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -20,10 +20,17 @@ import java.util.List;
 public class LargeTextController {
 
     private final LargeTextService largeTextService;
+    private final GPTService gptService;
 
     @PostMapping
     @Operation(summary = "대량 텍스트로 생성 API", description = "대량 텍스트 입력 시 자동으로 빵을 생성해주는 API 입니다.")
     public ApiResponse<List<CardResponse>> agreeTerms(@AuthUser User user, @RequestBody String text) {
         return ApiResponse.onSuccess(largeTextService.createCardList(text));
+    }
+
+    @PostMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @Operation(summary = "대량 텍스트로 생성 스트림 API", description = "대량 텍스트 입력 시 주제별로 하나씩 스트림 방식으로 반환하는 API 입니다.")
+    public Flux<CardResponse> streamCards(@AuthUser User user, @RequestBody String text) {
+        return largeTextService.createCardListStream(text);
     }
 }
