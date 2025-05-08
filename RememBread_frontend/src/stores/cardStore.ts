553ad3aaa7e28e11cd188a26d65878eb44a1fdcc
이성준 <akacast01@gmail.com>
2025-05-08 16:51:46 +0900

    [FE] Feat: Add Complete Empty Card Storage (S12P31A704-310)

--- RememBread_frontend/src/stores/cardStore.ts
+++ RememBread_frontend/src/stores/cardStore.ts
@@ -5,11 +5,15 @@ import { createEmptyCard } from "@/utils/createEmptyCard";
 interface CardStore {
   cardSet: indexCard[];
   setCardSet: (cards: indexCard[]) => void;
+  appendCard: (card: indexCard) => void;
   resetCardSet: () => void;
+  clearCardSet: () => void;
 }
 
 export const useCardStore = create<CardStore>((set) => ({
   cardSet: [createEmptyCard()],
   setCardSet: (cards) => set({ cardSet: cards }),
+  appendCard: (card) => set((state) => ({ cardSet: [...state.cardSet, card] })),
   resetCardSet: () => set({ cardSet: [createEmptyCard()] }),
+  clearCardSet: () => set({ cardSet: [] }),
 }));
