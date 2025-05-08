b5505eb53f63a5cc390a18ded807258e06163c50
송정호 <dynamite885@naver.com>
2025-05-08 13:33:08 +0900

    [BE] Feat: Add cardId field to CardResponse (S12P31A704-281)

--- be/remembread/src/main/java/com/remembread/card/converter/CardConverter.java
+++ be/remembread/src/main/java/com/remembread/card/converter/CardConverter.java
@@ -14,6 +14,7 @@ public class CardConverter {
 
     public static CardResponse toCardResponse(Card card) {
         CardResponse cardResponse = new CardResponse();
+        cardResponse.setCardId(card.getId());
         cardResponse.setNumber(card.getNumber());
         cardResponse.setConcept(card.getConcept());
         cardResponse.setDescription(card.getDescription());
