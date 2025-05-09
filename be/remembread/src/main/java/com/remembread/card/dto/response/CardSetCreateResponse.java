5a668a00a619c5712296ae0e92a2ffe27c1ffb19
송정호 <dynamite885@naver.com>
2025-05-09 13:35:16 +0900

    [BE] Feat: Add CardSetCreateResponse (S12P31A704-283)

---
+++ be/remembread/src/main/java/com/remembread/card/dto/response/CardSetCreateResponse.java
@@ -0,0 +1,14 @@
+package com.remembread.card.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CardSetCreateResponse {
+    private Long cardSetId;
+}
