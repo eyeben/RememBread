c4f285424dfe10c400e980d6bdd13c47a93ca3f0
Riis <gunil099@gmail.com>
2025-05-12 16:04:15 +0900

    [FE] Feat: Add FCM (S12P31A704-#334) - Add FCM

--- RememBread_frontend/src/main.tsx
+++ RememBread_frontend/src/main.tsx
@@ -1,6 +1,7 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import "@/lib/firebase/settingFCM";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "@/routes/router";
 import "@/index.css";
