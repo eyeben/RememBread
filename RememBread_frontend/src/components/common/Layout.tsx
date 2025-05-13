98a73c024599ec4d6d6339fd53a1c39cd17337ea
이성준 <akacast01@gmail.com>
2025-05-13 09:30:04 +0900

    [FE] Feat: Add global toaster to layout (S12P31A704-383)

--- RememBread_frontend/src/components/common/Layout.tsx
+++ RememBread_frontend/src/components/common/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet, useMatches } from "react-router-dom";
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
+import { Toaster } from "@/components/ui/toaster";
 
 interface RouteHandle {
   header?: boolean;
@@ -10,21 +11,25 @@ interface RouteHandle {
 const Layout = () => {
   const routeMatch = useMatches().find((match) => match.handle);
   const layoutConfig: RouteHandle = routeMatch?.handle || {};
-  const headerComponent = (layoutConfig.header ?? true) ? <Header /> : null;
-  const footerComponent = (layoutConfig.footer ?? true) ? <Footer /> : null;
+  const headerComponent = layoutConfig.header ?? true ? <Header /> : null;
+  const footerComponent = layoutConfig.footer ?? true ? <Footer /> : null;
 
   return (
-    <div className="flex flex-col min-w-[300px] w-full max-w-[600px] h-full min-h-screen mx-auto bg-white pc:border-x border-neutral-200">
-      {headerComponent}
-      <main
-        className={`flex flex-col flex-1 ${headerComponent ? "pt-14" : ""} ${
-          footerComponent ? "pb-16" : ""
-        }`}
-      >
-        <Outlet />
-      </main>
-      {footerComponent}
-    </div>
+    <>
+      <Toaster />
+      <div className="flex flex-col min-w-[300px] w-full max-w-[600px] h-full min-h-screen mx-auto bg-white pc:border-x border-neutral-200">
+        {headerComponent}
+        <main
+          className={`flex flex-col flex-1 ${headerComponent ? "pt-14" : ""} ${
+            footerComponent ? "pb-16" : ""
+          }`}
+        >
+          <Outlet />
+        </main>
+
+        {footerComponent}
+      </div>
+    </>
   );
 };
 
