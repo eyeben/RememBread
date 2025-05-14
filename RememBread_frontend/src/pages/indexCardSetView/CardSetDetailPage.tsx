941d0b817f94b38ac06bea09872390aac5b8cf2d
최원서 <cws98tiger@naver.com>
2025-05-14 17:44:26 +0900

    [FE] Feat: Add marker study line (S12P31A704-358)

--- RememBread_frontend/src/pages/indexCardSetView/CardSetDetailPage.tsx
+++ RememBread_frontend/src/pages/indexCardSetView/CardSetDetailPage.tsx
@@ -55,12 +55,12 @@ const CardSetDetailPage = () => {
 
   const handleStudyClick = async () => {
     try {
-      navigate("study", { state: { card: cardFromState } });
       const response = await startRecord(cardSetId, {
         mode: "STUDY",
         latitude: 0.1,
         longitude: 0.1,
       });
+      navigate("study", { state: { card: cardFromState } });
       console.log("학습 시작:", response);
       console.log("cardFromState", cardFromState);
     } catch (e) {
