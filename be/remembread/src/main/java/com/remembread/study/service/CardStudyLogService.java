5f17f65013778dd666c318d06f808bf09d5e32f4
송정호 <dynamite885@naver.com>
2025-05-16 15:15:47 +0900

    [BE] Feat: Add getDayLogResponses method to CardStudyLogService (S12P31A704-369)

--- be/remembread/src/main/java/com/remembread/study/service/CardStudyLogService.java
+++ be/remembread/src/main/java/com/remembread/study/service/CardStudyLogService.java
@@ -1,11 +1,14 @@
 package com.remembread.study.service;
 
+import com.remembread.study.converter.StudyConverter;
+import com.remembread.study.dto.response.SummaryLogResponse;
 import com.remembread.study.entity.CardStudyLog;
 import com.remembread.study.repository.CardStudyLogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -16,6 +19,13 @@ public class CardStudyLogService {
     @Transactional
     public void saveAll(List<CardStudyLog> cardStudyLogs) {
         cardStudyLogRepository.saveAll(cardStudyLogs);
+    }
+
+    @Transactional(readOnly = true)
+    public List<SummaryLogResponse.YearLogResponse.MonthLogResponse.DayLogResponse> getDayLogResponses(
+            LocalDate start, LocalDate end, Long userId
+    ) {
+        return StudyConverter.toDayLogResponseList(cardStudyLogRepository.findDailyStudyStatsByUserIdAndStudiedAtBetween(userId, start, end));
     }
 
 }
