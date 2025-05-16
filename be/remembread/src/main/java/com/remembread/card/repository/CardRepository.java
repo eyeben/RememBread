dd3d715a3409908e002c0dada5ac52b51bd68914
eyeben <qkrehdals97@naver.com>
2025-05-16 18:23:01 +0900

    [BE] Feat: Implement unlock (S12P31A704-271)
    implemented and tested

--- be/remembread/src/main/java/com/remembread/card/repository/CardRepository.java
+++ be/remembread/src/main/java/com/remembread/card/repository/CardRepository.java
@@ -6,6 +6,8 @@ import com.remembread.user.entity.User;
 import org.springframework.data.domain.Limit;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
