96b8d097eb0de350ffa491d49d7ae5d601afe975
송정호 <dynamite885@naver.com>
2025-05-16 15:14:37 +0900

    [BE] Feat: Add toSummaryLogResponse method to StudyConverter (S12P31A704-369)

--- be/remembread/src/main/java/com/remembread/study/converter/StudyConverter.java
+++ be/remembread/src/main/java/com/remembread/study/converter/StudyConverter.java
@@ -1,10 +1,12 @@
 package com.remembread.study.converter;
 
 import com.remembread.study.dto.response.RouteResponse;
+import com.remembread.study.dto.response.SummaryLogResponse;
 import com.remembread.study.entity.StudySession;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.LineString;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,6 +39,82 @@ public class StudyConverter {
         return RouteResponse.Route.builder()
                 .studiedAt(studySession.getStudiedAt())
                 .route(toRoute(studySession.getRoute()))
+                .build();
+    }
+
+    public static SummaryLogResponse toSummaryLogResponse(List<SummaryLogResponse.YearLogResponse.MonthLogResponse.DayLogResponse> dayLogResponses) {
+        Integer monthTotalCorrect = 0;
+        Integer monthTotalSolved = 0;
+        Integer yearTotalCorrect = 0;
+        Integer yearTotalSolved = 0;
+        Integer totalCorrect = 0;
+        Integer totalSolved = 0;
+        int lastYear = -1;
+        int lastMonth = -1;
+        List<SummaryLogResponse.YearLogResponse> years = new ArrayList<>();
+        List<SummaryLogResponse.YearLogResponse.MonthLogResponse> months = new ArrayList<>();
+        List<SummaryLogResponse.YearLogResponse.MonthLogResponse.DayLogResponse> days = new ArrayList<>();
+        for (SummaryLogResponse.YearLogResponse.MonthLogResponse.DayLogResponse dayLogResponse : dayLogResponses) {
+            LocalDate studiedAt = dayLogResponse.getDay();
+            int year = studiedAt.getYear();
+            int month = studiedAt.getMonthValue();
+            days.add(dayLogResponse);
+            monthTotalCorrect += dayLogResponse.getTotalCorrect();
+            monthTotalSolved += dayLogResponse.getTotalSolved();
+            if (lastMonth != month) {
+                SummaryLogResponse.YearLogResponse.MonthLogResponse monthLogResponse = SummaryLogResponse.YearLogResponse.MonthLogResponse.builder()
+                        .month(month)
+                        .totalCorrect(monthTotalCorrect)
+                        .totalSolved(monthTotalSolved)
+                        .days(days)
+                        .build();
+                months.add(monthLogResponse);
+                days = new ArrayList<>();
+                yearTotalCorrect += monthTotalCorrect;
+                yearTotalSolved += monthTotalSolved;
+                monthTotalCorrect = 0;
+                monthTotalSolved = 0;
+                lastMonth = month;
+            }
+            if (lastYear != year) {
+                SummaryLogResponse.YearLogResponse yearLogResponse = SummaryLogResponse.YearLogResponse.builder()
+                        .year(year)
+                        .totalCorrect(yearTotalCorrect)
+                        .totalSolved(yearTotalSolved)
+                        .months(months)
+                        .build();
+                years.add(yearLogResponse);
+                months = new ArrayList<>();
+                totalCorrect += yearTotalCorrect;
+                totalSolved += yearTotalSolved;
+                yearTotalCorrect = 0;
+                yearTotalSolved = 0;
+                lastYear = year;
+            }
+
+        }
+        if (lastMonth != -1) {
+            SummaryLogResponse.YearLogResponse.MonthLogResponse monthLogResponse = SummaryLogResponse.YearLogResponse.MonthLogResponse.builder()
+                    .month(lastMonth)
+                    .totalCorrect(monthTotalCorrect)
+                    .totalSolved(monthTotalSolved)
+                    .days(days)
+                    .build();
+            months.add(monthLogResponse);
+        }
+        if (lastYear != -1) {
+            SummaryLogResponse.YearLogResponse yearLogResponse = SummaryLogResponse.YearLogResponse.builder()
+                    .year(lastYear)
+                    .totalCorrect(yearTotalCorrect)
+                    .totalSolved(yearTotalSolved)
+                    .months(months)
+                    .build();
+            years.add(yearLogResponse);
+        }
+        return SummaryLogResponse.builder()
+                .totalCorrect(totalCorrect)
+                .totalSolved(totalSolved)
+                .years(years)
                 .build();
     }
 }
